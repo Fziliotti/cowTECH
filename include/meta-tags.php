d939376b88fa920a6a08fbd404fa6eb8f82336ac
Fabrício Fernandes Ziliotti <ffz.ziliotti@gmail.com>
2017-11-06 01:38:13 -0200

    organizando erro do github

--- include/meta-tags.php
+++ include/meta-tags.php
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-
 <?php 
 $systema = "cowTECH";
 
@@ -43,51 +41,3 @@ $systema = "cowTECH";
   <![endif]-->
 
 
-
-=======
-
-<?php 
-$systema = "cowTECH";
-
-?>
-<!-- NAO ALTERAR A SEQUENCIA -->
-<meta charset="utf-8">
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta http-equiv="content-language" content="pt-br">	
-<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-<meta name="description" content="">
-<meta name="author" content="Techmob empresa junior">
-<meta name="reply-to" content="ffz.ziliotti@gmail.com">
-<meta name="generator" content="Sublime Text">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<meta name="mobile-web-app-capable" content="yes">
-<link rel="manifest" href="manifest.json"> 
-<link rel="icon" sizes="192x192" href="../img/icon-196.png">
-<link rel="icon" sizes="128x128" href="../img/icon-128.png">
-<link rel="apple-touch-icon" sizes="128x128" href="../img/icon-128.png">
-<link rel="apple-touch-icon-precomposed" sizes="128x128" href="../img/icon-128.png"> 
-<link rel="stylesheet" href="../css/tether.min.css">
-<link rel="stylesheet" href="../css/bootstrap.min.css">
-<link rel="stylesheet" href="../css/font-awesome.min.css">
-<link rel="stylesheet" href="../css/ionicons.min.css">
-<link rel="stylesheet" href="../css/dataTables.bootstrap.min.css">
-<link rel="stylesheet" href="../css/AdminLTE.min.css">
-<link rel="stylesheet" href="../css/blue.css">
-<link rel="stylesheet" href="../css/skin-green.min.css">
-<link rel="stylesheet" href="../css/estilo.css">
-<script src="../js/sweetalert2.all.min.js"></script>
-<link rel="stylesheet" href="../css/sweetalert2.min.css">
-<title><?php echo $systema; ?></title>
-<!-- Google Font -->
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
-  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-  <!--[if lt IE 9]>
-  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-  <![endif]-->
-
-
-
->>>>>>> 08c187e95521aea907095fc5062ab83b3098a2de
--- include/scripts-footer.php
+++ include/scripts-footer.php
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 
 <!-- NAO ALTERAR A SEQUENCIA -->
 <script src="../js/jquery.min.js"></script>
@@ -19,27 +19,3 @@
 
 
 
-
-=======
-
-<!-- NAO ALTERAR A SEQUENCIA -->
-<script src="../js/jquery.min.js"></script>
-<!-- <script src="../js/tether.min.js"></script> -->
-<script src="../js/bootstrap.min.js"></script>
-<script src="../js/jquery.slimscroll.min.js"></script>
-<script src="../js/fastclick.js"></script>
-<script src="../js/adminlte.min.js"></script>
-<script src="../js/icheck.min.js"></script>
-<script src="../js/jquery.dataTables.min.js"></script>
-<script src="../js/dataTables.bootstrap.min.js"></script>
-
-
-
-
-
-<!--  <script src="../js/demo.js"></script> -->
-
-
-
-
->>>>>>> 08c187e95521aea907095fc5062ab83b3098a2de
